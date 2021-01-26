4f4928d00152c5e57e9e6550c353489f9b8e9c4d
Steve Miller <me@r15cookie.com>
2021-01-26 06:33:58 -0500

    Move to standard prompt ending

--- themes/font/font.theme.sh
+++ themes/font/font.theme.sh
@@ -59,7 +59,7 @@ function prompt_command() {
     # Append new history lines to history file
     history -a
 
-    PS1="$(clock_prompt)${virtualenv}${hostname} ${bold_cyan}\W $(scm_prompt_char_info)${bold_purple}$(kubernetes_context) ${ret_status}→ ${normal}"
+    PS1="$(clock_prompt)${virtualenv}${hostname} ${bold_cyan}\W $(scm_prompt_char_info)${bold_purple}$(kubernetes_context) ${ret_status}\$ ${normal}"
 }
 
 safe_append_prompt_command prompt_command
